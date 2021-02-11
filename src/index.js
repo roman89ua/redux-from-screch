bfb0893a6c09a9f309bca65671b9d5faea3a3771
roman89ua <roman89ua@gmail.com>
2021-02-11 13:44:35 +0200

    have created my own redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-import './styles.css'
+import './styles.css';
+import {createStore} from "./createStore";
+import {rootReducer} from "./redux/rootReducer";
+
+const counter = document.getElementById('counter');
+const addBtn = document.getElementById('add');
+const subBtn = document.getElementById('sub');
+const asyncBtn = document.getElementById('async');
+const themeBtn = document.getElementById('theme');
+
+
+const store = createStore(rootReducer, 0);
+
+window.store = store;
+
+addBtn.addEventListener('click', () => {
+    store.dispatch({type: 'INCREMENT'});
+    // counter.textContent = store.getState().toString();
+});
+subBtn.addEventListener('click', () => {
+    store.dispatch({type: 'DECREMENT'});
+    // counter.textContent = store.getState().toString();
+});
+asyncBtn.addEventListener('click', () => {
+
+});
+themeBtn.addEventListener('click', () => {
+    // document.body.classList.toggle('dark');
+});
+store.subscribe(()=>{
+    counter.textContent = store.getState().toString();
+})
+
+store.dispatch({type: '__INITIAL_STATE__'});
